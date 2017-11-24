6cc6ea1b182e9e7d9029742810b1b66e85a540bb
sorgelig <pour.garbage@gmail.com>
2017-11-25 02:41:38 +0800

    Load ROM from SD

---
+++ releases/build_rom_MrTNT.bat
@@ -0,0 +1,40 @@
+@echo off
+
+set    zip=mrtnt.zip
+set ifiles=tnt.1+tnt.2+tnt.3+tnt.4+tnt.1+tnt.2+tnt.3+tnt.4+tnt.5+tnt.6
+set  ofile=a.mrtnt.rom
+
+rem =====================================
+setlocal ENABLEDELAYEDEXPANSION
+
+set pwd=%~dp0
+echo.
+echo.
+
+if EXIST %zip% (
+
+	!pwd!7za x -otmp %zip%
+	if !ERRORLEVEL! EQU 0 ( 
+		cd tmp
+
+		copy /b/y %ifiles% !pwd!%ofile%
+		if !ERRORLEVEL! EQU 0 ( 
+			echo.
+			echo ** done **
+			echo.
+			echo Copy "%ofile%" into root of SD card
+		)
+		cd !pwd!
+		rmdir /s /q tmp
+	)
+
+) else (
+
+	echo Error: Cannot find "%zip%" file
+	echo.
+	echo Put "%zip%", "7za.exe" and "%~nx0" into the same directory
+)
+
+echo.
+echo.
+pause
